b68f397aad25c0fc0b688fbf74c2f16a0d136391
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-19 08:56:53 +0200

    init

---
+++ StringsDemo.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.strings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+
+    class StringsDemo
+    {
+        static void Main(string[] args)
+        {
+            string text = "Pesho";
+            Console.WriteLine (text.Length);
+
+        }
+    }
+}
